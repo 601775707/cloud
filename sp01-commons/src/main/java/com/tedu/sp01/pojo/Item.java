dbb07b974d3fde526363fc7a8074f846893ba288
KK <KK@kk>
2019-08-27 16:24:33 +0800

    test5

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Deprecated
+// 修改了
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Deprecated
+//修改了
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
