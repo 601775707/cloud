af2eb10aea57704aa8b7f9ffd991a65f96161c34
KK <KK@kk>
2019-08-27 16:27:16 +0800

    test6

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Order.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
 @Deprecated
-=======
->>>>>>> branch 'master' of https://github.com/601775707/cloud.git
 //修改了
 @Data
 @NoArgsConstructor
